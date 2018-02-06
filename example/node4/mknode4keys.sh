2670ca3b7aff1f8b4e192fb6c73680fb3e078ef7
Sleepy Senzu <tkyonezu@gmail.com>
2018-02-06 17:05:38 +0900

    Revise mknode4keys.sh for multi-instances.

--- example/node4/mknode4keys.sh
+++ example/node4/mknode4keys.sh
@@ -6,6 +6,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+if [ $# -ge 1 ]; then
+  INSTANCE_NO=$1
+else
+  INSTANCE_NO=${INSTANCE_NO:-4}
+fi
+
 if [ "$(uname -m)" = "armv7l" ]; then
   PROJECT=arm32v7
 else
@@ -14,7 +20,7 @@ fi
 
 docker run -t --rm --name iroha-pi -v $(pwd):/opt/iroha/config \
   ${PROJECT}/iroha-pi bash -c "cd /opt/iroha/config; \
-    for i in \$(seq 4) ; do
+    for i in \$(seq ${INSTANCE_NO}) ; do
       /opt/iroha/bin/iroha-cli --new_account --name node\${i} \
       --pass_phrase magicseed\${i}; \
     done"

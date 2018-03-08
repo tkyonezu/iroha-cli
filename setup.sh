efe712f4ff74639f8205cee531324ff9e5b8513e
Sleepy Senzu <tkyonezu@gmail.com>
2018-03-08 19:14:01 +0900

    Add setup.sh for Install on the Host.

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright 2018 Takeshi Yonezu. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+apt -y install cmake gcc g++ git python3 python3-pip
+apt -y clean
+
+git clone -b feature/remove-redis https://github.com/tkyonezu/iroha-cli.git \
+cd iroha-cli
+pip3 install -r requirements.txt
+python3 setup.py install
+python3 setup.py develop
+
+exit 0

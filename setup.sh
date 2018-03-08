bb956ea35c40ce743f4600e40b0b7966c3914b4e
Sleepy Senzu <tkyonezu@gmail.com>
2018-03-08 22:43:59 +0900

    Refactoring remove trailing backslash

--- setup.sh
+++ setup.sh
@@ -8,8 +8,10 @@
 apt -y install cmake gcc g++ git python3 python3-pip
 apt -y clean
 
-git clone -b feature/remove-redis https://github.com/tkyonezu/iroha-cli.git \
+git clone -b feature/remove-redis https://github.com/tkyonezu/iroha-cli.git
+
 cd iroha-cli
+
 pip3 install -r requirements.txt
 python3 setup.py install
 python3 setup.py develop
